c8e0fad7c1fe1b55e1b08442cd75a5521acdbf65
F4lc0131 <nicolaflavio22@gmail.com>
2021-01-04 16:00:28 +0100

    Open repo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using MSE.Assembly;
+using MSE;
+using MSE.instructionSets;
+
+namespace ASMToMSE32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input file here: ");
+            Converter converter = new Converter(Console.ReadLine());
+            Console.Write("Input where to store the output here: ");
+            File.WriteAllBytes(Console.ReadLine(), converter.Compile());
+            Console.WriteLine("Compile complete!");
+        }
+    }
+}
